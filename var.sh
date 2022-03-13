d783396d1ec748b34bc2b507df0deba516db0fca
kumarankitgupta <ankitkrgupta149@gmail.com>
2022-03-13 17:10:22 +0530

    variables in shell

---
+++ var.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#  for comment use has symbol
+# variable in bash :-
+
+# System Variables
+echo Bash name $BASH
+echo Bash Version $BASHVERSION
+echo $HOME
+echo working directory $PWD
+
+#user variables
+val=10
+name=Mark
+echo $val
+echo "my name is " $name
