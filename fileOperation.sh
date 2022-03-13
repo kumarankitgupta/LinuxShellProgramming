ea77cb8cb44f72ebdbe57c01ad09a0bea4fe60f2
kumarankitgupta <ankitkrgupta149@gmail.com>
2022-03-13 20:07:44 +0530

    file realated operation

--- fileOperation.sh
+++ fileOperation.sh
@@ -20,12 +20,19 @@ else
 fi
 echo
  # to check wheather a file is Block Special file or Charcter Special file
-read -p "Enter file name : " file_name
+read -p "Enter file name to check Block/Character files : " file_name
 if [ -b $file_name ]
 then
     echo "Block Special file"
 else
     echo "Charcter Special file"
 fi
-
+# to check wheather a file is empty or not
+read -p "Enter file name to check it is empty or not : " file_name
+if [ -s $file_name ]
+then
+    echo "The File is not empty"
+else
+    echo "The file is Empty"
+fi
 
