63eebf778d630cf7f7e72dce4d26cabbe7a13a54
kumarankitgupta <ankitkrgupta149@gmail.com>
2022-03-17 09:23:25 +0530

    minor changes

--- maths.sh
+++ maths.sh
@@ -8,3 +8,6 @@ echo "diff =" $(( $num1 - $num2 ))
 echo "prod =" $(( $num1 * $num2 ))
 echo "div  =" $(( $num1 / $num2 ))
 echo "mod  =" $(( $num1 % $num2 ))
+echo
+read -p "enter two number " num3 num4
+echo "$num3+$num4" | bc
